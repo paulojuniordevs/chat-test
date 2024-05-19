0ef8e953dfbd708b610d578be0d755fd7a2f8a3e
Paulo <84019548+paulojuniordevs@users.noreply.github.com>
2024-05-19 00:26:14 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("wss://chat-test-4i4s.onrender.com")
+    websocket = new WebSocket("wss://chat-test-toub.onrender.com")
     websocket.onmessage = processMessage
 }
 
